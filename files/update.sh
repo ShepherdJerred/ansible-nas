86afbba4523da75df89d058fa59f426810e14032
Jerred Shepherd <ShepherdJerred@gmail.com>
2021-01-03 11:52:23 -0800

    Move more configuration to ansible

--- files/update.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-$(cd /server/data/plugins &&
-  rm -f MyFirstPlugin.dll &&
-  wget https://github.com/shepherdjerred-imposter/my-first-plugin/releases/download/tip/MyFirstPlugin.dll
-  )
