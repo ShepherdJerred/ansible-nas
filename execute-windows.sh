c0070beb698e71123fc0026cb9bb98a9d6294591
Jerred Shepherd <ShepherdJerred@gmail.com>
2021-01-12 21:38:36 -0800

    More MacBook setup

--- execute-windows.sh
+++ execute-windows.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ansible-playbook main.yml -i production/inventory.yml -e@vars/vaulted_vars.yml --limit macbook-pro.home.shepherdjerred.com --ask-become-pass --ask-vault-pass
+ansible-playbook main.yml -i production/inventory.yml -e@vars/vaulted_vars.yml --limit macbook-pro.home.shepherdjerred.com --ask-vault-pass
